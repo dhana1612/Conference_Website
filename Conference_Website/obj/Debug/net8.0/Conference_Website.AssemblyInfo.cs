70896e2f3fc9bf35e49547dbc023e9929f446394
Dhana <dhana121622@gmail.com>
2025-02-10 22:40:39 +0530

    commit

--- Conference_Website/obj/Debug/net8.0/Conference_Website.AssemblyInfo.cs
+++ Conference_Website/obj/Debug/net8.0/Conference_Website.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Conference_Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+440a70e3bfa0a40a1251ecbfe7a5bb73b6f4a14f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60697685d19fd6981eff7dbe6d3c9701244e8eff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Conference_Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Conference_Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
